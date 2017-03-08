0f8f2153da54c709d8456a682ce0fee7ffee3e74
Maxwell Benton <maxwellbenton@gmail.com>
2017-03-08 20:37:54 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  arr = []
+  array.each do |element|
+    element = element.to_i ** 2
+    arr.push(element)
+  end
+  arr
 end
+
+arr = [0,1,2,3,4,7]
+square_array(arr)
