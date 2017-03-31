51406020ea9a5f98811b0b85d8cd82b6755f39ac
Mike Luo <zluo@alumni.berklee.edu>
2017-03-31 16:33:27 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  squared = []
+  array.each {|n| squared.push(n**2)}
+  return squared
 end
+
+# def square_array(array)
+#   array.collect {|n| n**2}
+# end
