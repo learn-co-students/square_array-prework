d968206842ffe046b878985af830e2c55983ddad
Jason  Rosso <jasonrosso@gmail.com>
2017-04-14 16:22:05 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,21 @@
-=begindef square_array(numbers)
+
+#METHOD WITH REGULAR .EACH LOOP
+def square_array(numbers)
 empty = []
   numbers.each do |x|
     empty.push(x*x)
   end
   return empty
 end
-=end
 
+#METHOD WITH REGULAR .EACH LOOP AND USE OF SHOVEL INTO ARRAY
+def square_array(array)
+  squared = []
+  array.each { |x| squared << x ** 2 }
+  squared
+end
+
+#METHOD WITH COLLECT
 def square_array(numbers)
-numbers.collect{|x|x*x}
+numbers.collect {|x|x*x}
 end
