26dab5282cf3199ddc2fa0fc27c84b86e31796bb
Jason  Rosso <jasonrosso@gmail.com>
2017-04-14 16:17:20 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
-def square_array(numbers)
+=begindef square_array(numbers)
 empty = []
   numbers.each do |x|
     empty.push(x*x)
   end
   return empty
+end
+=end
+
+def square_array(numbers)
+numbers.collect{|x|x*x}
 end
