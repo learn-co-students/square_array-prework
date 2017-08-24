9dcdae77886686bba5447b35fb863ab7e57de22f
Robert Peck <rspeckpfs@gmail.com>
2017-08-24 19:02:28 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  new_array = Array.new
+  array.each do |item|
+    new_array << item**2
+  end
+  new_array
 end
+
+# def square_array(array) #This works in the irb, but the tests seems to check for 'each' so it fails the tests.
+#   array.collect{|item| item**2}
+# end
