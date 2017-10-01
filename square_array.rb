367f658b220434a070d57c46c677fec76d6f7860
github username <github email address>
2017-09-30 20:39:16 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,8 @@ def square_array(array)
   squaredArray = []
 
   array.each do |numbers|
-    squaredArray.push(numbers**2) 
+    squaredArray.push(numbers**2)
   end
     puts squaredArray.size()
     return squaredArray
 end
-square_array([1, 2, 3])
