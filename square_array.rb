0bc355a61ebcfa075ac2e091a74e78f4913a0be1
mmb16 <mmb583@nyu.edu>
2017-10-10 05:52:50 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+#  array_size = array.size
+#  iterations = 0
+#    while iterations < array_size
+#      array[iterations] = array[iterations] ** 2
+#      iterations+=1
+#    end
+arr = []
+array.each do |element|
+  arr << element ** 2
+end
+arr
 end
