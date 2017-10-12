d29ee49a91e0dcaa4e841f54a417a5b16608ee83
Kelly S <kellydsample@gmail.com>
2017-10-12 20:28:19 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   # your code here
+  final = []
+
+  array.each do |number|
+    final.push(number * number)
+  end
+  return final 
 end
+
+# basket.each do |apple|
+#     puts "Taking out #{apple}"
+# end
