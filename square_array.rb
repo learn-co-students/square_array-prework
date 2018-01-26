c6b4b922b46dbd2ffb2cc657ea64620864ece311
lidianvilazio <lidianvilazio@hotmail.com>
2018-01-26 12:08:36 -0500

    My Square Array

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+  n_array = []
+  array.each do |n|
+      n_array.push(n**2)
+  end
+  n_array
 end
+
+#collect option :)
+#def square_array(array)
+#    n_array = array.collect {|n| n**2}
+#end
+
