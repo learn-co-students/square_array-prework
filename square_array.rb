8a9f53e6be12d37ee977624081fbfeb191ed051d
github username <github email address>
2018-01-29 13:00:01 -0500

    each w/ each, collect, and inject

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+
 def square_array(array)
-  # your code here
+new_array = []
+array.each{ |value| new_array.push(value * value)}
+return new_array
+
 end
+
+
+#array.collect{|value| value * value }
+#array.inject{|acc = Array.new, value| return acc.push(value *value)}
