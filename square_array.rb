5ace00ee01f2be3b5814da746f59842048aee025
Harim Choi <harim1206@gmail.com>
2018-02-08 17:18:46 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
 def square_array(array)
   # your code here
+
+
+  array.each do |item|
+    item ** 2
+  end
+
+  
+  i = 0
+
+  while i < array.size
+    array[i] = array[i]**2
+    i += 1
+  end
+
+  array
+
 end
