ecec63785fe1ba3ba297ebc068803582e5f1f776
victoria-huang <vctrah@gmail.com>
2018-02-09 13:28:17 -0500

    each method

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  
+  array.each do |num|
+      squared = num ** 2
+      new_array << squared
+  end
+  
+  new_array
 end
+
+# def square_array_with_collect(array)
+#    new_array = array.collect {|num| num ** 2}
+# end
