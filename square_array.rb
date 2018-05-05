8c9b2528d3735afeacf69be0c6def01f285f1562
Alberto Carreras <a.carreras.c@gmail.com>
2018-05-04 23:51:09 -0400

    Solved lab

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  square_array = []
+  array.each {|number| square_array << number**2} 
+  square_array
 end
+
+=begin
+1.take each value from square_array
+2.square it
+3.push or shove them into a new array
+=end
