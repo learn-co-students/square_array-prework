f04fc94e0acf2e4784a058bd667b1fc34f06d6b7
Alberto Carreras <a.carreras.c@gmail.com>
2018-05-04 23:59:10 -0400

    Added .collect in a new method

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,15 @@
 def square_array(array)
   square_array = []
-  array.each {|number| square_array << number**2} 
+  array.each {|number| square_array << number**2}
   square_array
 end
+
+def square_array_with_collect(array)
+
+  array.collect {|number| number**2}
+
+end
+
 
 =begin
 1.take each value from square_array
