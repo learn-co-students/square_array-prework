7c02e549d6a8c706074bf248401e67394758a75a
cmcorrales <cmcorrales3@gmail.com>
2018-06-05 06:31:56 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,17 +1,4 @@
 
-# def square_array(array)
-# # your code here
-#   length = array.size
-#   count = 0
-#   new_array = []
-#   while count < length
-#     new_item = array[count] ** 2
-#     count += 1
-#     new_array.push(new_item)
-#   end
-#   new_array
-# end
-
 def square_array(array)
 new_array = []
   array.each do |item|
