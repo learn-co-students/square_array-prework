9c8431ee7b3a1784aa337957a31272b793396d35
cmcorrales <cmcorrales3@gmail.com>
2018-06-05 06:31:29 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,22 @@
+
+# def square_array(array)
+# # your code here
+#   length = array.size
+#   count = 0
+#   new_array = []
+#   while count < length
+#     new_item = array[count] ** 2
+#     count += 1
+#     new_array.push(new_item)
+#   end
+#   new_array
+# end
+
 def square_array(array)
-  # your code here
+new_array = []
+  array.each do |item|
+    new_item = item ** 2
+    new_array.push(new_item)
+  end
+  new_array
 end
