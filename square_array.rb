5bf83d16571ddb2888f64799ff88b71082d3ba64
Your Name <you@example.com>
2018-06-16 00:22:30 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+new_array = []
+  #the code asks  us to have a new array to return sqaured
+  #numbers
+
+  array.each do |number|
+  number =  number * number
+    new_array.push(number)
+end
+return new_array
 end
