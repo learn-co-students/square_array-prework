d101ba850285cfccce085448bf13af741a574304
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-07-13 18:41:46 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -6,13 +6,6 @@ def square_array(array)
     new_array.push(num**2)
   end 
 
-  new_array 
+  return new_array 
   
 end
-
-##  my_statements = []
-#   toppings.each do |topping|
-#     my_statements << "I love #{topping} on my burgers!"
-#     end
-#     my_statements
-#   end
