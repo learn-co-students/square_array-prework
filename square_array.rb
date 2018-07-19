2d65a1ff9222d494c66df1268e81ee6c45bf80e5
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-19 22:56:30 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,53 @@
 def square_array(array)
-  # your code here
   squared = []
-  array.each do |num| 
-    squared << num ** 2 
+  array.each do |element|
+    squared << element ** 2 
   end
   squared
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def square_array(array)
+#   # your code here
+#   squared = []
+#   array.each do |num| 
+#     squared << num ** 2 
+#   end
+#   squared
+# end
