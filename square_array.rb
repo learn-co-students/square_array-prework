f11945b100f1bf2ccadba1f069042a7ac14202c8
github username <github email address>
2018-09-14 11:36:48 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,11 @@
-def square_array(array)
-  # your code here
-  squared_nums = [] # saving the new squared values
-  array.each do |i|
-    squared_nums << i ** 2
-  end
-    return squared_nums
-end
+# def square_array(array)
+#   # your code here
+#   squared_nums = [] # saving the new squared values
+#   array.each do |i|
+#     squared_nums << i ** 2
+#   end
+#     return squared_nums
+# end
 
 
 # def square_array(array)
@@ -13,3 +13,10 @@ end
 #   array.collect {|i| squared_nums<<i ** 2}
 #   squared_nums
 # end
+
+
+def square_array(array)
+  squared_nums = []
+  array.each {|i| squared_nums << i ** 2}
+  return squared_nums
+end
