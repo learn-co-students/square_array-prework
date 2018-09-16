9d1798543510ddca60efb9253b20e1a4eb27efc3
Irfan Suleman <ivsuleman@gmail.com>
2018-09-16 23:20:06 +0100

    fixed

--- square_array.rb
+++ square_array.rb
@@ -7,3 +7,22 @@ array.each {|x| new_array.push(x ** 2)}
 return new_array
 
 end
+
+=begin
+count = 0
+new_array = []
+
+loop do 
+
+  new_array.push(array[count] ** 2)
+
+  if count  == array.length - 1
+    break
+  end
+
+  count += 1
+
+end
+
+return new_array
+=end
