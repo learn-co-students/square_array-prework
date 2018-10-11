b49fdfb17c7d4993bcbe0cba48d891066a0a259a
Robert Schulte <schulte.robertw@outlook.com>
2018-10-10 21:12:09 -0500

    defined array iterator methods

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,21 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |item|
+    new_array.push(item ** 2)
+  end
+  new_array
 end
+
+def new_square_array(array)
+  array.map do |item|
+    item ** 2
+  end
+end
+
+numbers = [1,2,3]
+
+puts square_array(numbers)
+
+new_numbers = [9,10,16,25]
+
+puts new_square_array(new_numbers)
