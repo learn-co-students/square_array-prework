5a9dfece2f936a111d119cc7b8def8b841eac842
github username <github email address>
2018-10-17 18:54:50 -0400

    My first commit

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   # your code here
+  arr = []
+  array.each do |number|
+    arr << number**2
+  end
+  arr
 end
+
+# Please note that #each simply returns the original array. 
+# You will need to store the squared numbers into an array, 
+# so that upon squaring each element, it'll be pushed into 
+# another array for later. 
