7dd699f558e6b81c421ae101e06b19236bd1eef0
github username <crishanks12@gmail.com>
2018-10-27 20:49:15 -0600

    Done.

--- square_array.rb
+++ square_array.rb
@@ -6,3 +6,10 @@ def square_array(array)
   result
 end
 square_array([1,2,3])
+
+def square_array_with_collect(array)
+  array.collect do |item|
+    item ** 2
+  end
+end
+square_array_with_collect([1,2,3])
