1ee01a6bb3d604c7696f97d0f6852040c5aac887
Michael Evans <mmevansjr@gmail.com>
2019-06-27 16:03:42 -0700

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+    squared_array = []
+    array.each {|i| squared_array.push(i**2)}
+    return squared_array
 end
+
+# in this lab, I was asked to take the numbers from an array and square the values
+# and output them into a new array using the .each method.
+
